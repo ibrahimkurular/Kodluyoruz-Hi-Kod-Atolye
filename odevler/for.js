7d0d6a9d316f68182b05d1d501423c1103f5aa94
İbrahim Kurular <57796847+ibrahimkurular@users.noreply.github.com>
2025-02-05 01:30:01 +0300

    Add files via upload

---
+++ odevler/for.js
@@ -0,0 +1,5 @@
+// • For döngüsü kullanarak 1'den 10'a kadar rakamları konsola yazdırın.
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
---
+++ odevler/operator.js
@@ -0,0 +1,16 @@
+// • Operatörleri kullanarak bir matematiksel işlem yazıp konsola yazdırın.
+
+let sayi1 = 30;
+let sayi2 = 17;
+
+let toplam = sayi1 + sayi2;
+let cikarma = sayi1 - sayi2;
+let carpma = sayi1 * sayi2;
+let bolme = sayi1 / sayi2;
+let kalan = sayi1 % sayi2;
+
+console.log("Toplam:" + toplam);
+console.log("Çıkarma:" + cikarma);
+console.log("Çarpma:" + carpma);
+console.log("Bölme:" + bolme);
+console.log("Kalan:" + kalan);
